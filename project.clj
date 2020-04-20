d82ceec0b3425303da5fb44b6d533203a1933aeb
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2020-04-20 12:08:37 -0300

    Add coverage and test runner

--- project.clj
+++ project.clj
@@ -2,8 +2,11 @@
   :description "My Clojure learning repository"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
+  :plugins [
+            [lein-cloverage "1.1.2"]
+            [lein-eftest "0.5.9"]]
   :main ^:skip-aot learning-clojure.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
