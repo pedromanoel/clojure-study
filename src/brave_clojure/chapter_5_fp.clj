2e74d6e3f22ff0fb72c664d62e5dae2dd6947044
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2020-04-30 17:57:57 -0300

    Add chapter 5 exercises about composition

--- src/brave_clojure/chapter_5_fp.clj
+++ src/brave_clojure/chapter_5_fp.clj
@@ -1,8 +1,47 @@
 (ns brave-clojure.chapter-5-fp)
+(require '[clojure.string :as s])
 
 (defn sum
+  "Example using recursion for looping in a sequence"
   ([vals] (sum vals 0))
   ([[first & rest] accumulating-sum]
    (if (nil? first)
      accumulating-sum
      (recur rest (+ accumulating-sum first)))))
+
+(defn clean
+  "Example using function composition f(g(x))"
+  [text]
+  (s/trim (s/replace text #"lol" "LOL")))
+
+(defn clean-thread
+  "Example using function composition with thread call"
+  [text]
+  (-> text
+      (s/replace,,, #"lol" "LOL")
+      (s/trim)))
+
+(defn clean-comp
+  "Example of function composition using (comp f g)"
+  [text]
+  ((comp s/trim s/replace) text #"lol" "LOL"))
+
+; Examples using comp to retrieve deep values from data
+(def c-int (comp :intelligence :attributes))
+(def c-str (comp :strength :attributes))
+(def c-dex (comp :dexterity :attributes))
+
+(defn spell-slots
+  "calculate spell slots with formula floor(1 + intelligence/2"
+  [char]
+  (int (inc (/ (c-int char) 2))))
+
+(defn spell-slots-comp
+  "calculate spell slots using comp"
+  [char]
+  ((comp int inc #(/ % 2) c-int) char))
+
+(defn my-comp
+  "implement a composition function"
+  ([f] (fn [args] apply f args))
+  ([f g] (f g)))
--- test/brave_clojure/chapter_5_fp_test.clj
+++ test/brave_clojure/chapter_5_fp_test.clj
@@ -1,6 +1,27 @@
 (ns brave-clojure.chapter-5-fp-test
   (:require [clojure.test :refer :all])
-  (:require [brave-clojure.chapter-5-fp :refer [sum]]))
+  (:require [brave-clojure.chapter-5-fp :refer :all]))
 
 (deftest sum-test
   (is (= 10 (sum [1 2 3 4]))))
+
+(deftest clean-test
+  (is (= "LOL" (clean "  lol  ")))
+  (is (= "LOL" (clean-thread "  lol  ")))
+  (is (= "LOL" (clean-comp "  lol  "))))
+
+(deftest comp-test
+  (let [character {:name       "Smooches McCutes"
+                   :attributes {
+                                :intelligence 10
+                                :strength     4
+                                :dexterity    5}}]
+    (is (= 10 (c-int character)))
+    (is (= 4 (c-str character)))
+    (is (= 5 (c-dex character)))
+    (is (= 6 (spell-slots character)))
+    (is (= 6 (spell-slots-comp character)))))
+
+
+(deftest my-comp-test
+  (is (= -8 ((my-comp inc -) 9))))
