83465d5a1d4c3e225052daa6361093ce44d6b7e6
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2020-05-05 18:16:03 -0300

    Create and exercise Patient record

--- src/learning_clojure_alura/lesson1.clj
+++ src/learning_clojure_alura/lesson1.clj
@@ -2,9 +2,13 @@
 
 ; example of patients list
 ; { 15 { :id 15 :name "Patient 15" }, 23 { :id 23 :name "Patient 23" } }
+(defrecord Patient [id name birth-date])
 
 (defn add-patient
   [patient patients]
   (if-let [id (:id patient)]
-      (assoc patients id patient)
-      (throw (ex-info "Patient missing ID" {:patient patient}))))
+    (assoc patients id patient)
+    (throw (ex-info "Patient missing ID" {:patient patient}))))
+
+(seq (map->Patient {:name "Name" :declared-gender "Fox"}))
+
--- test/learning_clojure_alura/lesson1_test.clj
+++ test/learning_clojure_alura/lesson1_test.clj
@@ -1,11 +1,61 @@
 (ns learning-clojure-alura.lesson1-test
   (:require [clojure.test :refer :all])
-  (:require [learning-clojure-alura.lesson1 :refer [add-patient]])
-  (:import (clojure.lang ExceptionInfo)))
+  (:require [learning-clojure-alura.lesson1 :refer :all])
+  (:import (clojure.lang ExceptionInfo)
+           (learning_clojure_alura.lesson1 Patient)))
+
+(def ID 1)
+(def NAME "Name")
+(def BIRTH-DATE "01/01/1950")
+(def DOG-NAME "Fluffy")
+
+(deftest record-test
+  (testing "Records"
+    (testing "with positional construction"
+      (let [patient (Patient. ID NAME BIRTH-DATE)]
+        (is (= ID (:id patient)))
+        (is (= NAME (:name patient)))
+        (is (= BIRTH-DATE (:birth-date patient)))))
+    (testing "with map construction"
+      (testing "passing extra attributes"
+        (let [patient (map->Patient
+                        {:id         ID
+                         :name       NAME
+                         :birth-date BIRTH-DATE
+                         :dog-name   DOG-NAME})]
+          (is (= DOG-NAME (:dog-name patient)))))
+      (testing "leaving attributes out"
+        (let [patient (map->Patient {:birth-date BIRTH-DATE})]
+          (is (nil? (:id patient)))
+          (is (nil? (:name patient))))))
+    (testing "check if record"
+      (is (record? (map->Patient {}))))
+    (let [patient (map->Patient
+                    {:id         ID
+                     :name       NAME
+                     :birth-date BIRTH-DATE})]
+      (testing "Record acts like a map"
+        (testing "access attributes with call"
+          (is (= ID (:id patient))))
+        (testing "access attributes with get"
+          (is (= NAME (get patient :name))))
+        (testing "when used with seq"
+          (is (= [[:id ID]
+                  [:name NAME]
+                  [:birth-date BIRTH-DATE]]
+                 (seq patient))))
+        (testing "when assoc new attribute"
+          (is (= DOG-NAME (:dog-name (assoc patient :dog-name DOG-NAME)))))
+        (testing "when assoc existing attribute"
+          (is (= "Someone Else" (:name (assoc patient :name "Someone Else")))))
+        (testing "when dissoc attribute"
+          (is (nil? (:id (dissoc patient :id))))))
+      (testing "Record can act as a java class"
+        (is (= BIRTH-DATE (.birth_date patient)))))))
 
 (deftest add-patient-test
-  (let [william {:id 15 :name "Patient 15"}
-        unidentified {:name "Unidentified Patient"}
+  (let [william (->Patient 15 "Patient 15" "01/01/1984")
+        unidentified (map->Patient {:name "Unidentified Patient"})
         patients {}]
     (testing "Add patient to patients collection"
       (is (= {15 william}
